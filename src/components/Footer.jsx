6fe7d052e20cda7109fe60fa6dff9238f9639b2b
Xiaochen Lu <xiaoclu@ebay.com>
2023-04-29 21:42:00 +0800

    fix: fixed the footer layout out of screen issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,26 +11,30 @@ export default function Footer({ layoutHeight }) {
   return (
     <FooterAntd
       style={{
+        fontSize: "0.75rem",
         textAlign: "center",
+        paddingTop: "1.8vh",
         height: layoutHeight.footer,
         color: "rgba(0, 0, 0, 0.6)",
         backgroundColor: "rgba(0, 0, 0, 0.05)",
       }}
     >
-      <Row style={{ marginTop: "-2vh" }}>
+      DATS-SHU 235 Final Project Group 3, Spring 2023 by Xiaochen (Nigel) Lu &amp; Xingyu (Addison)
+      Guo
+      {/* <div style={{ fontSize: "1vh", top: "-1vh" }}>
+        <b>DATS-SHU 235 Final Project Group 3, Spring 2023</b> by <em>Xiaochen (Nigel) Lu</em> &amp;{" "}
+        <em>Xingyu (Addison) Guo</em>
+      </div> */}
+      {/* <Row style={{ position: "absolute", top: `95.5vh`, left: "45vw" }}>
         <Col span={24}>
-          <span style={{ fontSize: "8px" }}>
+          <span style={{ fontSize: "0.75vh" }}>
             by <em>Xiaochen (Nigel) Lu</em> &amp; <em>Xingyu (Addison) Guo</em>
           </span>
-        </Col>
-      </Row>
-      <Row style={{ paddingTop: "0.3rem", marginTop: "-0.75vh" }}>
-        <Col span={24}>
-          <span style={{ fontSize: "10px" }}>
+          <span style={{ fontSize: "1vh" }}>
             <b>DATS-SHU 235 Final Project Group 3, Spring 2023</b>
           </span>
         </Col>
-      </Row>
+      </Row> */}
     </FooterAntd>
   );
 }
